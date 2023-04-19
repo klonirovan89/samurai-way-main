fde56fc88dea16f35a55e7655f4e7f6dcf52200f
klonirovan89 <klonirovan89@gmail.com>
2023-04-19 16:01:36 +0300

    Change readme

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+    return(
+        <div>
+            <a href='#s'>Home</a>
+            <a href='#s'>News Feed</a>
+            <a href='#s'>Mes sages</a>
+        </div>
+    );
+}
+
+
+export default Header;
---
+++ src/Technologies.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+const Technologies = () => {
+    return (
+    <div>
+            <div >one div</div>
+            <div className="App">
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
+    </div>
+    );
+}
+
+
+export default Technologies;
