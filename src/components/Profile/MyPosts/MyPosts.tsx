f022d4c1edbbb1ac4d6ac1cacda3cae296d8a1be
klonirovan89 <klonirovan89@gmail.com>
2023-07-10 22:29:10 +0300

    Commit - 5

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -7,19 +7,27 @@ type MyPostsType = {
     posts: MyPostsPropsType[]
 }
 
-const MyPosts = (props:MyPostsType) => {
+const MyPosts = (props: MyPostsType) => {
 
-     let postsElements = props.posts.map(p => <Post id={p.id} message={p.message} likesCount={p.likesCount}/>)
+    let postsElements = props.posts.map(p => <Post id={p.id} message={p.message} likesCount={p.likesCount}/>)
+
+    let newPostElement = React.createRef<HTMLTextAreaElement>()
+
+    const addPost = () => {
+        let text = newPostElement.current?.value
+        alert(text)
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
+
                 </div>
             </div>
             <div className={s.posts}>
